b878dd2fd3e0c59195683ecc2679a353f2277065
etsybaev <etsybaev@gmail.com>
2017-06-08 14:47:19 +0300

    Added EmailScheduleServiceImp class (No implementation yet)

---
+++ src/main/java/com/fgvmonserv/service/mail/EmailScheduleServiceImp.java
@@ -0,0 +1,37 @@
+package com.fgvmonserv.service.mail;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+
+/**
+ * Created by ievgeniit on 29.05.17.
+ */
+
+
+@Configuration
+@EnableScheduling
+public class EmailScheduleServiceImp {
+
+//    // once every hour, on top of the hour, Mondays to Fridays
+//    @Scheduled(cron = "* 0 * * * MON-FRI")
+//    public void sendMail() {
+//
+//    }
+
+    // once every minute, on top of the hour, Mondays to Fridays
+    @Scheduled(cron = "0 * * * * MON-FRI")
+    public void print() {
+
+        System.out.println("==================================================================================");
+        System.out.println("==================================================================================");
+
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        System.out.println("==================================================================================");
+        System.out.println("==================================================================================");
+
+    }
+
+}
--- src/main/java/com/fgvmonserv/service/mail/JavaGmailSSLExample.java
+++ src/main/java/com/fgvmonserv/service/mail/JavaGmailSSLExample.java
@@ -31,6 +31,5 @@ public class JavaGmailSSLExample {
 
         System.out.println("done.");
     }
-
 }
 
